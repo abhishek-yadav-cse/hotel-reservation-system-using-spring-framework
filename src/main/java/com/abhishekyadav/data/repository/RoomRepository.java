b194365122d24872acece536860ba88ea0692ee6
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-10-02 19:03:20 -0700

    building the RESTful service

---
+++ src/main/java/com/abhishekyadav/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.abhishekyadav.data.repository;
+
+import com.abhishekyadav.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long>{
+    Room findByNumber(String number);
+}
